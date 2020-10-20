5dcca3449d9139fb58aee7d0fbe4724d107c9f66
pbajie <jingyuanp7@gmail.com>
2020-10-20 03:29:16 -0500

    up

---
+++ src/app/flight/flight.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+export class Flight {
+  public originalfight: string;
+  public destinationfight: string;
+
+  public regdate: Date;
+  public nums: number;
+
+
+}
+
+@Component({
+  selector: 'app-flight',
+  templateUrl: './flight.component_r6.html',
+  styleUrls: ['./flight.component.css']
+})
+export class FlightComponent implements OnInit {
+
+  constructor() { }
+  model = new Flight();
+  fg: string;
+  ngOnInit(): void {
+  }
+
+
+  onSubmit(form): void {
+    this.fg = 'flight price:' + (form.value.nums * 150) + '$';
+    console.log(form.value.nums * 150 + '$');
+  }
+}
