36c5f46096649df3bffb0f16f93e27ee7d529632
pbajie <jingyuanp7@gmail.com>
2020-11-03 02:42:06 -0600

    update: hw3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 import { FlightComponent } from './flight/flight.component';
 import {FormsModule} from '@angular/forms';
+const routes: Routes = [
+  {path: 'flightInfo', component:FlightComponent},
+  {path: 'about', component:AboutComponent}
+
+];
 @NgModule({
   declarations: [
     AppComponent,
     FlightComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(routes),
     FormsModule
   ],
   providers: [],
